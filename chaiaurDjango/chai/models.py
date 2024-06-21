9a2395babb8e28910d61f5ef6645964ed200d984
Anoop Sharma <anoopsharma873682@gmail.com>
2024-06-21 21:15:52 +0530

    Handle model and url in django

--- chaiaurDjango/chai/models.py
+++ chaiaurDjango/chai/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class ChaiVarity(models.Model):
+    ChaiType = [
+        ('ML', 'Masala'),
+        ('GR', 'Ginger'),
+        ('KI', 'Kiwi'),
+        ('PL', 'Plain'),
+        ('EL', 'Elaichi'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chai/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=ChaiType)
+    description  = models.TextField(default='')
+
+    def __str__(self):
+        return self.name
+    
